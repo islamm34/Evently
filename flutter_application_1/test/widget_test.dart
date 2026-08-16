a0c7ac121331028b6d98dcccee75482a35d7d879
Islam Mohamed <https://github.com/islamm34>
2026-08-17 01:34:26 +0300

    Create App Splash

--- flutter_application_1/test/widget_test.dart
+++ flutter_application_1/test/widget_test.dart
@@ -1,12 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:qora/app/app.dart';
 
 void main() {
   testWidgets('App loads role selection screen', (WidgetTester tester) async {
-    await tester.pumpWidget(const QoraApp());
+    await tester.pumpWidget(const CoffeeShopApp());
     await tester.pumpAndSettle();
 
-    expect(find.text('Choose Your Role'), findsOneWidget);
+    expect(find.text('Welcome to Qora'), findsOneWidget);
   });
 }
